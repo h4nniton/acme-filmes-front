ab54decb78849b3509b0103a5864ed893454a629
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2024-03-26 11:21:03 -0300

    terminar

---
+++ js/filmes.js
@@ -0,0 +1,33 @@
+export async function getFilmes () {
+    const url = 'http://10.107.134.43:8080/v2/acme/filmes'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.filmes
+}
+
+export async function getFilme (id) {
+    const url = `http://10.107.134.43:8080/v2/acme/filme/${id}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.filme[0]
+}
+
+export async function postFilme (filme) {
+    const url = `http://10.107.134.43:8080/v2/acmefilmes/filme`
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(filme),
+    }
+
+    fetch (url, options)
+
+    const response = await fetch (url, options)
+
+    return response.ok
+
+}
---
+++ js/main.js
@@ -0,0 +1,53 @@
+import { getFilmes, getFilme, postFilme } from "./filmes.js";
+
+function criarCard (filme) {
+    const card = document.createElement('div')
+    const titulo = document.createElement('h2')
+    titulo.textContent = filme.nome 
+
+    const valor = document.createElement('button')
+    valor.textContent = filme.valor_unitario
+
+    card.append(titulo, valor)
+
+    return card 
+}
+
+async function preencherContainer () {
+
+    const container = document.querySelector('body')
+    const filme = await getFilmes()
+
+    const filmes = [
+        {
+        nome: "Paranorman"
+        },
+
+        {
+            nome: "Sociedade da neve"
+        }
+    
+    ]
+
+    filmes.forEach ( filme => {
+        const card = criarCard (filme)
+        container.appendChild (card)
+    })
+    
+}
+
+preencherContainer()
+
+// TESTE
+
+// filme = {
+//     "nome": "Avatar",
+//     "sinopse": "Homem azul seila seila seila",
+//     "duracao": "02:10:00",
+//     "data_lancamento":"2021-12-15",
+//     "data_relancamento":null,
+//     "foto_capa":"https://exemplo.com/foto_vale.jpg",
+//     "valor_unitario":49.9900016784668
+// }
+
+// postFilme(filme)
